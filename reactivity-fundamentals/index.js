eb39327b85228fe43deb45a6bf70f63501ecd420
蔡武 <caiwu@3xdata.cn>
2022-03-07 02:26:09 +0800

    init

--- reactivity-fundamentals/index.js
+++ reactivity-fundamentals/index.js
@@ -1,50 +1,81 @@
 let activeEffct = null
-
+const effctStack = []
 const bucket = new WeakMap()
-function effect(fn) {
-  activeEffct = fn
-  fn()
+
+function effect (fn) {
+  const effectFn = () => {
+    celanup(effectFn)
+    activeEffct = effectFn
+    effctStack.push(effectFn)
+    fn()
+    effctStack.pop()
+    activeEffct = effctStack[effctStack.length - 1]
+  }
+  effectFn.deps = []
+  effectFn()
+
 }
-function trigger(target, key) {
+
+function celanup (effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i]
+    deps.delete(effectFn)
+  }
+  effectFn.deps.length = 0
+}
+
+function trigger (target, key) {
   const depsMap = bucket.get(target)
   if (!depsMap) return
   const deps = depsMap.get(key)
-  deps && deps.forEach((fn) => fn())
+  const depsToRun = new Set()
+  deps.forEach(effectFn => {
+    if (activeEffct !== effectFn) {
+      depsToRun.add(effectFn)
+    }
+  })
+  console.log(depsToRun);
+  depsToRun && depsToRun.forEach((effectFn) => effectFn())
 }
-function track(target, key) {
+
+function track (target, key) {
   if (!activeEffct) return
   let depsMap = bucket.get(target)
+  console.log(depsMap);
   if (!depsMap) {
     bucket.set(target, (depsMap = new Map()))
   }
   let deps = depsMap.get(key)
+  console.log(deps);
   if (!deps) {
     depsMap.set(key, (deps = new Set()))
   }
   deps.add(activeEffct)
+  activeEffct.deps.push(deps)
 }
-function ref(data) {
+
+function defineReactive (data) {
   return new Proxy(data, {
-    get(target, key) {
+    get (target, key) {
       track(target, key)
       return Reflect.get(target, key)
     },
-    set(target, key, value) {
+    set (target, key, value) {
       Reflect.set(target, key, value)
       trigger(target, key)
     },
   })
 }
-const data = ref({
-  name: 'li ning',
-  age: 22,
-})
-effect(() => {
-  console.log(`my name is ${data.name}`)
-})
-effect(() => {
-  console.log(`my age is ${data.age}`)
-})
 
-data.name = 'caiwu'
-data.age = 11
+const data = defineReactive({
+  value1: 'value1',
+  value2: 'value2'
+})
+effect(() => {
+  console.log('执行了f1:');
+  effect(() => {
+    console.log('执行了f2:');
+    const a = data.value2
+  })
+  const a = data.value1
+})
